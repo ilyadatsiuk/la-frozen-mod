367ce0f1a37cb3703c899b0483849320d27c7c79
Ilya Datsiuk <ilya.datsiuk@gmail.com>
2024-03-17 18:52:05 +0100

    Changes to be committed:

--- remappedSrc/com/datdevelop/mixin/ExampleMixin.java
+++ remappedSrc/com/datdevelop/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.datdevelop.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
--- src/client/java/com/datdevelop/MaksimGayClient.java
+++ src/client/java/com/datdevelop/MaksimGayClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.datdevelop;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class MaksimGayClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
